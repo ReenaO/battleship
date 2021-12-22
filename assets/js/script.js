0cbe19dbcec85e08ab12d3226c4e841e15835f27
ReenaO <reena.mccarthy@hotmail.com>
2021-12-22 23:33:38 +0000

    Add grids to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+
+let gameArray1 = [`A, B, C, D, E, F, G, H, I, J`];
+  for (let i=0; i<=9; i++);
+   console.log(gameArray1)
+
+let gameArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  for (let i=1; i<=10; i++) {
+      console.log(gameArray2);
+}
+
+let myGrid = gameArray1 + gameArray2;
+  console.log(myGrid);
+  
+let oppenentComputerGrid =  gameArray1 + gameArray2;
+  console.log(oppenentComputerGrid);
+
+
